8336fa8a3973f5b123a93a1ef5d41e4540bc4423
Faisak1420 <Fysalfysal16@gmail.com>
2018-04-08 22:27:16 +0300

    1222

--- src/main/java/app/Calc.java
+++ src/main/java/app/Calc.java
@@ -1,5 +1,5 @@
 package app;
-
+ /* لرفع تاملف على الموقع اضغط على versino control كلك يمين على  Default  وبعدها اضغط على  commit وبعدها اضغط على commit and Pash*/
 public class Calc {
 
     public static void main(String[] args) {
